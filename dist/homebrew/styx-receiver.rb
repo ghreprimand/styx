7078e5cf750c145c7834cce9ee83abac8518791c
ghreprimand <203024559+ghreprimand@users.noreply.github.com>
2026-04-15 06:25:23 -0500

    Pin v0.5.0 homebrew formula sha256 to real tarball hash

--- dist/homebrew/styx-receiver.rb
+++ dist/homebrew/styx-receiver.rb
@@ -2,7 +2,7 @@ class StyxReceiver < Formula
   desc "Software KVM receiver for macOS -- receives keyboard/mouse from a Hyprland Linux machine"
   homepage "https://github.com/ghreprimand/styx"
   url "https://github.com/ghreprimand/styx/archive/refs/tags/v0.5.0.tar.gz"
-  sha256 "SKIP"
+  sha256 "21cae643cde50efe120181ab7d2822b0e90abba426760ced78a9c1e377a22bc3"
   license "MIT"
 
   depends_on "rust" => :build
