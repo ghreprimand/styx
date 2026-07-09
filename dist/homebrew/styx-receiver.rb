eed16abf095bb3058c359921a5e7631921f4cc19
ghreprimand <203024559+ghreprimand@users.noreply.github.com>
2026-07-09 11:52:45 -0500

    Point homebrew formula at v0.5.7 tarball

--- dist/homebrew/styx-receiver.rb
+++ dist/homebrew/styx-receiver.rb
@@ -1,8 +1,8 @@
 class StyxReceiver < Formula
   desc "Software KVM receiver for macOS -- receives keyboard/mouse from a Hyprland Linux machine"
   homepage "https://github.com/ghreprimand/styx"
-  url "https://github.com/ghreprimand/styx/archive/refs/tags/v0.5.6.tar.gz"
-  sha256 "b8660688c7e180fba64e3c2366fd2de0e12371863ffb1bedf441fb52c8ea6106"
+  url "https://github.com/ghreprimand/styx/archive/refs/tags/v0.5.7.tar.gz"
+  sha256 "d87812fbaea39f541bd43ee039da994a98509664902a2db89fdade313b8ffc52"
   license "GPL-3.0-or-later"
 
   depends_on "rust" => :build
